1111e04db765a4cf38c54ffd4060760e161fb8ef
maddojka <yuriy.soroko@gmail.com>
2024-04-14 21:43:49 +0300

    Server-client architecture - hometask

--- client/src/com/soroko/client/ClientApp.java
+++ client/src/com/soroko/client/ClientApp.java
@@ -6,5 +6,6 @@ public class ClientApp {
     public static void main(String[] args) {
         new Client(new InetSocketAddress("127.0.0.1", 2222))
                 .startClient();
+        System.out.println();
     }
 }
